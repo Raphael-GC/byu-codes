7409e4111bc3598be3656512c971088775ec8107
Raphael-GC <raphaelcarneirodev@gmail.com>
2024-07-04 12:28:17 -0300

    :sparkles: feat: Add the midweek exercises.

---
+++ web-and-computer-programming/cse-110/week-1/clever_stories.py
@@ -0,0 +1,20 @@
+print("Please enter the following information: ")
+
+# Ask for the basic information
+adjective_1 = input("adjective: ")
+animal = input("animal: ")
+verb_1 = input("verb: ")
+exclamation = input("exclamation: ")
+verb_2 = input("verb: ")
+verb_3 = input("verb: ")
+# My contribution to the story
+verb_4 = input("verb: ")
+noun = input("noun: ")
+adverb_1 = input("adverb: ")
+adverb_2 = input("adverb: ")
+
+# Showing the story
+print("Your story is: ")
+print(f"The other day, I was really in trouble. It all started when I saw a very {adjective_1.lower()} {animal.lower()} {verb_1.lower()} down the hallway. '{exclamation.capitalize()}!' I yelled. But all I could think to do was to {verb_2.lower()} over and over. Miraculously, that caused it to stop, but not before it tried to {verb_3.lower()} right in front of my family.")
+# Adding my closing of this story
+print(f"\nEmbarrassed beyond words, I {verb_4.lower()} there mortified as my {noun.lower()} stared at the scene in disbelief. I {adverb_1.lower()} scrambled to salvage the situation, cleaning up the mess and apologizing {adverb_2.lower()}.")
